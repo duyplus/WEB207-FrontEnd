615c5f843a62aad356534cdc89a2b97b818109a5
DUYPLUS <duyplusdz@gmail.com>
2022-03-28 21:08:43 +0700

    remove

--- Lab/WEB207_Lab7/js/ex3.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("LabSPA", ["ngRoute"]);
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when("/thethao", { templateUrl: "./pages/tinthethao.html" })
-        .when("/giaoduc", { templateUrl: "./pages/tingiaoduc.html" })
-        .when("/xahoi", { templateUrl: "./pages/tinxahoi.html" })
-        .when("/thohay", { templateUrl: "./pages/thohay.html" })
-        .otherwise({ template: "<h3>Đây là trang chủ</h3>" })
-});
--- Lab/WEB207_Lab7/js/tin/thohay.js
+++
@@ -1,9 +0,0 @@
-[
-    { "idTin": "104", "TieuDe": "Bài thơ Tiếng Thu của Lưu Trọng Lư ", "Ngay": "2008-12-30", "Xem": "266", "TomTat": "Xuất hiện giữa mùa hạ nóng bức và mùa đông lạnh lẽo, mùa thu mát dịu và mơ màng là nguồn cảm hứng bất tận của văn nhân, thi sĩ xưa nay. Viết Thu vịnh và Đây mùa thu tới, Nguyễn Khuyến và Xuân Diệu đã đóng góp cho văn học sử nước nhà hai bài thơ hay về cảnh thu và tình thu. " },
-    { "idTin": "105", "TieuDe": "Lời vàng của sếp", "Ngay": "2008-12-30", "Xem": "442", "TomTat": "Bây giờ ghế ít đít nhiều Chúng mày hãy nhớ những điều sau đây" },
-    { "idTin": "106", "TieuDe": "Bình khô đập chén cả cười với nhau ", "Ngay": "2008-12-30", "Xem": "411", "TomTat": "Hôm nay ánh trăng vàng lơ LỬNG Gió nhẹ lay hờ HỮNG cành CÂY Đắng cay lời cuối là ĐÂY Ngày mai xa cách cho ĐẦY nhớ THƯƠNG(*)" },
-    { "idTin": "107", "TieuDe": "Ngày Xưa Dư Âm...", "Ngay": "2008-12-31", "Xem": "231", "TomTat": "Thơ thất ngôn độc thanh: Đây là một đặc dạng của thơ thất ngôn tứ tuyệt, đặc biệt chỉ dùng thanh bằng, tạo nên âm điệu lạ  " },
-    { "idTin": "126", "TieuDe": "Yêu", "Ngay": "2009-01-01", "Xem": "468", "TomTat": "Yêu là chết ở trong lòng một ítVì mấy khi yêu mà chắc được yêu.Cho rất nhiều song nhận chẳng bao nhiêuNgười ta phụ, hoặc thờ ơ, chẳng biết ..." },
-    { "idTin": "127", "TieuDe": "BÀi thơ có 8 cách đọc", "Ngay": "2009-01-01", "Xem": "1", "TomTat": "Đây là một bài thơ thật kỳ lạ. Có đến 8 cách đọc khác nhau" },
-    { "idTin": "765", "TieuDe": "Bài thơ Ông đồ", "Ngay": "2011-07-06", "Xem": "0", "TomTat": "Mỗi năm hoa đào nở, lại thấy ông đồ già...Ở Việt Nam, liên quan đến đề tài Tết, có lẽ không có bài thơ nào được phổ biến rộng rãi cho bằng bài “Ông Đồ” của Vũ Đình Liên (1913-96)" }
-]
